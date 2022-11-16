773199a2d96e1c20864ffe835ccfcad1d6bef003
DiyaShibu1 <>
2022-11-16 22:33:24 +0000

    changed name

--- .ccls-cache/@home@runner@DifficultDefensiveDeadcode/main.c
+++ .ccls-cache/@home@runner@DifficultDefensiveDeadcode/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 void reed_ekidan2() {
-  printf("Diya Shibu and Prince Sonani\n");
+  printf("Diya Shibu and Prince Sona\n");
   printf("We learned that git and GitHub are amazing, powerful tools!\n");
 }
 
