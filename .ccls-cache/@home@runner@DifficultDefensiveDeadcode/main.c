5088757bf00d4a9eb1785e4a3bb5283c60647763
DiyaShibu1 <>
2022-11-16 22:38:29 +0000

    changed nothing

--- .ccls-cache/@home@runner@DifficultDefensiveDeadcode/main.c
+++ .ccls-cache/@home@runner@DifficultDefensiveDeadcode/main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 void reed_ekidan2() {
-  printf("Diya Shibu and Prince Sona\n");
+  printf("Diya Shibu and Prince Sonani\n");
   printf("We learned that git and GitHub are amazing, powerful tools!\n");
 }
 
 int main()
 {
-    printf("This code has been created by the students of CS 211, Fall 2022!\n\n");
+    printf("This code has been ceated by the students of CS 211, Fall 2022!\n\n");
     reed_ekidan2();
 
     return 0;
